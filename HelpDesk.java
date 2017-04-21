5ee8dce740b50294a87873fbff460c21edd2aa5f
Matteo Wong <Matteo@Matteos-MacBook-Air.local>
2017-04-20 21:40:48 -0400

    HelpDesk skeleton

---
+++ HelpDesk.java
@@ -0,0 +1,12 @@
+import cs1.Keyboard;
+
+public class HelpDesk
+{
+
+    private static ArrayPriorityQueue<Ticket> _line = new ArrayPriorityQueue<Ticket>;
+
+    public static void main(String[] args) {
+
+    }
+
+}
